80909fd7ec3c10580daffccbc5ddb4cf75cd2152
Sita <sitavemuri8@gmail.com>
2023-05-29 17:38:57 -0700

    Fix overflow

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" style={{scrollBehavior:'smooth'}}>
+    <Html lang="en" style={{scrollBehavior:'smooth', overflow: 'visible'}}>
       <Head />
       <body>
         <Main />
