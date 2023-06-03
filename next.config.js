1564fc8c96a45293a274869661a6a466f6c3d599
Sita <sitavemuri8@gmail.com>
2023-06-03 00:03:39 -0700

    Add waitlist

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/register',
+        source: '/waitlist',
         destination: 'https://docs.google.com/forms/d/e/1FAIpQLSdt9uy6hXMq6LT0aJKJZWOW96olBscj69L01gvKTW_sj5HOAA/viewform?usp=sf_link',
         permanent: false,
       },
@@ -14,7 +14,7 @@ module.exports = {
         source: '/prospectus',
         destination: 'https://drive.google.com/file/d/1ur_ZsYZbZkSXKHa9fWo_soazs_In-4Ez/view',
         permanent: false,
-      },
+      }
     ]
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -55,8 +55,8 @@ export default function Site() {
               <FaRegEnvelope size={48} />
             </a>
           </div>
-          <Link href="/register" scroll={false} className="mt-6 p-4 border-4 rounded-xl border-black dark:border-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-black">
-            <p className="text-2x md:text-3xl lg:text-4xl">Register!</p>
+          <Link href="/waitlist" scroll={false} className="mt-6 p-4 border-4 rounded-xl border-black dark:border-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-black">
+            <p className="text-2x md:text-3xl lg:text-4xl">Join the Waitlist!</p>
           </Link>
           {/* <Link href="#info" scroll={false} className="scroll-smooth"> */}
             <FaLongArrowAltDown className="mt-4" size={64} />
