7bde13781eade5b8a9f870fc3e96b44ef8a24f18
Sita <sitavemuri8@gmail.com>
2023-08-13 10:33:57 -0700

    Cover image source

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,13 @@ export default function Site() {
       </Head>
       <main>
         {/* <NavBar></NavBar> */}
-        <section className="w-screen h-screen bg-no-repeat bg-cover bg-center bg-fixed bg-none" style={{ backgroundImage: `url(/photos/1.jpg)` }}>
+        {/* <section className="w-screen h-screen bg-no-repeat bg-cover bg-center bg-fixed md:bg-[url('/photos/1.jpg')]"> */}
+        <section className="w-screen h-screen">
+          <Image className="-z-50 object-cover"
+            src={`/photos/1.jpg`}
+            alt="HackaKhan Photograph"
+            fill
+          />
           {/* <div className="w-screen fixed top-0 text-center opacity-90 hidden md:block lg:block bg-black dark:bg-white">
             <p className="my-4 text-base text-white md:text-lg lg:text-xl dark:text-black">Want to support HackaKhan? Read our <a href="/prospectus" target="_blank" rel="noreferrer" aria-label="Prospectus" className="underline">sponsorship prospectus</a>.</p>
           </div> */}
