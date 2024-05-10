48b9e242767a7cc73d35ba0b1115243b89db01c8
Sita <sitavemuri8@gmail.com>
2024-05-10 13:57:37 -0700

    Add a sponsor

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,30 +1,23 @@
 import Image from 'next/image'
 import KLS from '/public/sponsors/kls.png'
-import Wolfram from '/public/sponsors/wolfram.png'
 import VaSi from '/public/sponsors/vasi.png'
-import Echo from '/public/sponsors/echo3d.png'
+import Manifold from '/public/sponsors/manifold.png'
 
 export default function Sponsors() {
     return (
         <div className="bg-white dark:bg-black px-4 py-2.5 flex flex-col justify-center items-center">
-            <h2 className="mb-4 text-center text-xl md:text-2xl lg:text-3xl">Past Sponsors</h2>
-            <div className="grid grid-cols-4 p-4 gap-4 items-center justify-center dark:bg-white rounded-xl">
-                <a href="https://www.wolframalpha.com/" target="_blank" rel="noreferrer">
-                    <Image className="w-full"
-                        src={Wolfram}
-                        alt="Wolfram Logo"
-                    />
-                </a>
+            <h2 className="mb-4 text-center text-xl md:text-2xl lg:text-3xl">Sponsors</h2>
+            <div className="grid grid-cols-3 p-4 gap-4 items-center justify-center dark:bg-white rounded-xl">
                 <a href="https://vasi.ai/" target="_blank" rel="noreferrer">
                     <Image className="w-full"
                         src={VaSi}
                         alt="VaSi Logo"
                     />
                 </a>
-                <a href="https://www.echo3d.com/" target="_blank" rel="noreferrer">
+                <a href="https://www.manifoldrg.com/" target="_blank" rel="noreferrer">
                     <Image className="w-full"
-                        src={Echo}
-                        alt="Echo3D Logo"
+                        src={Manifold}
+                        alt="Manifold Logo"
                     />
                 </a>
                 <a href="https://khanlabschool.org/" target="_blank" rel="noreferrer">
