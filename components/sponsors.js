061597efb2e2b1d1b336926dcd7aeef5021c87a8
Sita <sitavemuri8@gmail.com>
2024-05-18 21:39:55 -0700

    A sponsor returns!

--- components/sponsors.js
+++ components/sponsors.js
@@ -4,12 +4,13 @@ import VaSi from '/public/sponsors/vasi.png'
 import Manifold from '/public/sponsors/manifold.png'
 import XYZ from '/public/sponsors/xyz.png'
 import AoPS from '/public/sponsors/aops.png'
+import Wolfram from '/public/sponsors/wolfram.png'
 
 export default function Sponsors() {
     return (
         <div className="bg-white dark:bg-black px-4 py-2.5 flex flex-col justify-center items-center">
             <h2 className="mb-4 text-center text-xl md:text-2xl lg:text-3xl">Sponsors</h2>
-            <div className="grid grid-cols-5 p-4 gap-4 items-center justify-center dark:bg-white rounded-xl">
+            <div className="grid grid-cols-3 grid-rows-2 p-4 gap-4 items-center justify-center dark:bg-white rounded-xl">
                 <a href="https://vasi.ai/" target="_blank" rel="noreferrer">
                     <Image className="w-full"
                         src={VaSi}
@@ -20,6 +21,12 @@ export default function Sponsors() {
                     <Image className="w-full"
                         src={Manifold}
                         alt="Manifold Logo"
+                    />
+                </a>
+                <a href="https://www.wolframalpha.com/" target="_blank" rel="noreferrer">
+                    <Image className="w-full"
+                        src={Wolfram}
+                        alt="Wolfram Logo"
                     />
                 </a>
                 <a href="https://gen.xyz/" target="_blank" rel="noreferrer">
