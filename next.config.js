5854466983c2f63651397737745f654f658197cf
Sita <sitavemuri8@gmail.com>
2024-06-03 15:31:29 -0700

    Switch to waitlist

--- next.config.js
+++ next.config.js
@@ -5,16 +5,21 @@ module.exports = {
 
   async redirects() {
     return [
-      {
-        source: '/register',
-        destination: 'https://docs.google.com/forms/d/e/1FAIpQLSf2nxLNU3rzZ-kpiV19vN6uXCsjRBuPSfLY1my7Zhe_rpVHJg/viewform?usp=sf_link',
-        permanent: false,
-      },
       // {
-      //   source: '/discord',
-      //   destination: 'https://discord.gg/AyEycmKE',
+      //   source: '/register',
+      //   destination: 'https://docs.google.com/forms/d/e/1FAIpQLSf2nxLNU3rzZ-kpiV19vN6uXCsjRBuPSfLY1my7Zhe_rpVHJg/viewform?usp=sf_link',
       //   permanent: false,
       // },
+      {
+        source: '/waitlist',
+        destination: 'https://forms.gle/oWA7iKGEg8WUSXbp8',
+        permanent: false,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/P7kwYHAEM6',
+        permanent: false,
+      },
       // {
       //   source: '/rubric',
       //   destination: 'https://docs.google.com/forms/d/e/1FAIpQLScdQU6D-vqAOQyHoW37foBeUwNXCHJJ6k65XnrAmfe49Oorlg/viewform?usp=sf_link',
