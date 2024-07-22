8a2bd62ee505da22fbe0a2fb6db9b60fa8b4bf9f
Sita <sitavemuri8@gmail.com>
2024-07-21 21:31:51 -0700

    Reset webpage

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,9 @@ export default function Site() {
       </Head>
       <main>
         <section className="w-screen h-screen">
-          <div className="w-screen fixed top-0 text-center opacity-90 hidden md:block lg:block bg-black dark:bg-white z-10">
+          {/* <div className="w-screen fixed top-0 text-center opacity-90 hidden md:block lg:block bg-black dark:bg-white z-10">
             <p className="my-4 text-base text-white md:text-lg lg:text-xl dark:text-black">Want to support HackaKhan? Read our <a href="/prospectus.pdf" target="_blank" rel="noreferrer" aria-label="Prospectus" className="underline">sponsorship prospectus</a>.</p>
-          </div>
+          </div> */}
           <div className="hidden md:block md:w-screen md:h-screen md:fixed md:-z-10">
             <Image
               src={'/photos/1.JPG'}
@@ -44,14 +44,14 @@ export default function Site() {
               alt="HackaKhan Logo"
             />
             <h1 className="mb-4 text-5xl font-bold md:text-6xl lg:text-7xl">HackaKhan</h1>
-            <div className="mb-4 text-center text-xl md:text-2xl lg:text-3xl">
+            {/* <div className="mb-4 text-center text-xl md:text-2xl lg:text-3xl">
               <Typewriter onInit={(typewriter) => {
                 typewriter
                   .typeString("June 8 2024").start()
                   .pauseFor(500)
                   .typeString("<br>Mountain View CA").start()
               }} />
-            </div>
+            </div> */}
             <div className="flex justify-center items-center gap-3 mb-4 h-auto">
               <a href="https://www.instagram.com/hackakhan_" target="_blank" rel="noreferrer" aria-label="Instagram">
                 <FaInstagram size={48} />
@@ -63,11 +63,11 @@ export default function Site() {
             {/* <Link href="/waitlist" scroll={false} className="mt-6 p-4 border-4 rounded-xl border-black dark:border-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-black">
               <p className="text-2x md:text-3xl lg:text-4xl">Join the Waitlist!</p>
             </Link> */}
-            <FaLongArrowAltDown className="mt-4" size={64} />
+            {/* <FaLongArrowAltDown className="mt-4" size={64} /> */}
           </div>
         </section>
         {/* <hr className="w-screen h-2 bg-black dark:bg-white" /> */}
-        <section className="w-screen bg-white dark:bg-black">
+        {/* <section className="w-screen bg-white dark:bg-black">
           <div className="w-screen h-max mb-4 md:mb-8 flex flex-col gap-8 items-center justify-center px-2 md:px-20 md:flex-row md:items-start">
             <About></About>
             <Sponsors></Sponsors>
@@ -76,7 +76,7 @@ export default function Site() {
             <Schedule></Schedule>
             <Map></Map>
           </div>
-        </section>
+        </section> */}
         <footer className="w-screen text-center bg-white dark:bg-black dark:text-white fixed bottom-0 py-4 hidden md:block md:text-lg lg:text-xl">&copy; {new Date().getFullYear()} HackaKhan. All Rights Reserved.</footer>
       </main>
     </>
